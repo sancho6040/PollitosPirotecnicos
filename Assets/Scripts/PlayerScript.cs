5ff7cf058d30d70b8035c6c25936a00333798e3e
sancho6040 <qwetito@gmail.com>
2023-06-05 06:35:58 -0500

    tentacle working
    +chicken wip
    +spawner wip
    
    -particles
    -sounds
    -update models
    -implement ui

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,16 +8,19 @@ public class PlayerScript : MonoBehaviour
     [Header("Player variables")]
     public int lives;
     public int score;
+    private bool bIsAttacking;
+    public float coolDown;
 
     [Header("movement variables")]
     public float movementSpeed = 1f;
     public float tentacleSpeed = 0.5f;
-    
+
     // tentacle
     private GameObject target;
     private GameObject tentacle;
     public GameObject tip;
-    
+    private ChickenDetector chikenDetector;
+
     //Scale values
     private float initialDistance;
     private float initialScale;
@@ -27,26 +30,52 @@ public class PlayerScript : MonoBehaviour
 
     private Rigidbody rb;
 
+    [Header("partiles")]
+
+    public GameObject crashParticles;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         target = GameObject.Find("Target");
         tentacle = GameObject.Find("Player/Tentacle");
+        chikenDetector = tip.GetComponent<ChickenDetector>();
 
         initialDistance = (transform.position - tip.transform.position).magnitude;
-        initialScale = tentacle.transform.localScale.z;
+        initialScale = tentacle.transform.localScale.x;
+
+        coolDown = 0f;
     }
 
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (coolDown > 0)
         {
-            tip.transform.DOMove(target.transform.position, tentacleSpeed).SetEase(Ease.OutQuart).OnComplete(() => playerMove());
+            coolDown -= Time.deltaTime;
+        }
+
+        if (Input.GetButton("Fire1") && coolDown <= 0)
+        {
+            Debug.Log("ola");
+            tip.transform.DOMove(target.transform.position, tentacleSpeed).SetEase(Ease.InQuart).OnComplete(() => playerMove());
+            coolDown = tentacleSpeed;
+            bIsAttacking = true;
+        }
+
+        if (!bIsAttacking)
+        {
+            tip.transform.DOMove(transform.position, tentacleSpeed).SetEase(Ease.OutQuart);
         }
 
         newDistance = (transform.position - tip.transform.position).magnitude;
         newScale = (newDistance * initialScale) / initialDistance;
-        
+
+        if (newDistance <= 0.2f && coolDown <= 0f)
+        {
+            bIsAttacking = false;
+        }
+
         tentacle.transform.DOScaleZ(newScale, 0f);
         tentacle.transform.LookAt(tip.transform);
     }
@@ -54,8 +83,43 @@ public class PlayerScript : MonoBehaviour
     void playerMove()
     {
         Vector3 unitDirection = (target.transform.position - transform.position).normalized;
-        Debug.Log(unitDirection);
         rb.velocity = Vector3.zero;
-        rb.AddForce(unitDirection * movementSpeed);
+
+        if (chikenDetector.isAChicken())
+        {
+            rb.AddForce(unitDirection * movementSpeed);
+        }
+        else
+        {
+            bIsAttacking = false;
+            rb.AddForce(-unitDirection * (movementSpeed / 3));
+        }
     }
+
+    void TakeDamage()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Chicken"))
+        {
+            if (bIsAttacking)
+            {
+                bIsAttacking = false;
+                //particles ????
+                // Instantiate(crashParticles, transform.position, Quaternion.identity);
+                //sound
+                score += 10;
+                other.gameObject.GetComponent<ChickenScript>().die();
+            }
+            else
+            {
+                TakeDamage();
+            }
+
+        }
+    }
+
 }
